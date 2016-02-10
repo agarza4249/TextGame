3e83a230f9da739dd7b5e6b9832ddbdc0a388159
agarza4249 <agarza4249@001LAB153010809.neisd.ad>
2016-02-10 16:02:56 -0600

    cewl

--- src/player.java
+++ src/player.java
@@ -10,13 +10,14 @@ public class player {
 	public static int apPotions = 1;
 	
 	//Player equippable items 
-	public static int sword = 0;
-	public static int shield = 0;
-	public static int armor = 0;
+	public static boolean sword = false;
+	public static boolean shield = false;
+	public static boolean armor = false;
 	
 	
 	public static void PlayerStats() {
 		
+		//show hp counter
 		System.out.println("With an hp of " + hp + " you feel ");
 		
 		if (hp > 80)
@@ -39,8 +40,37 @@ public class player {
 			System.out.print("near death.");
 		}
 		
-		System.out.println("");
+		//show items in inventory 
+		System.out.println("You currently own:");
 		
+		if(hpPotions > 0)
+		{
+			System.out.println(hpPotions + " health potions");
+		}
+		else if(apPotions > 0)
+		{
+			System.out.println(apPotions + " Action potions");
+		}
+		else if(sword = true)
+		{
+			System.out.println("A sword");
+		}
+		else if(shield = true)
+		{
+			System.out.println("A shield");
+		}
+		else if(armor = true)
+		{
+			System.out.println("Some armor");
+		}
+		else 
+		{
+			System.out.println("you own nothing to your name.");
+		}
+	
+	
+	
+	
 	}
 	
 	
